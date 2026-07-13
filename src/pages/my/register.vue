dea2545ca44b467df42b25ed66799e5228d46339
chenxiaomin <3314996791@qq.com>
2026-07-13 15:45:52 +0800

    url地址

--- src/pages/my/register.vue
+++ src/pages/my/register.vue
@@ -131,7 +131,7 @@ const handleRegister = () => {
   uni.showLoading({ title: '注册中...' });
 
   uni.request({
-    url: 'http://172.20.10.2:8083/api/login/register',
+    url: 'https://cxm.juntaitec.cn/miniprogram/api/login/register',
     method: 'POST',
     header: {
       'Content-Type': 'application/json'
