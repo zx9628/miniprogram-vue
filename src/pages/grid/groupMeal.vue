ceddf2f7790306d0eec968bf61f2800dda77a0a6
25927 <2592769572@qq.com>
2026-07-13 11:34:45 +0800

    首页跳转，及各个简陋的跳转页面

---
+++ src/pages/grid/groupMeal.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="content">
+    <text>拼团点餐页面正在开发中...</text>
+  </view>
+</template>
+
+<script setup>
+// 拼团逻辑后续在这里写
+</script>
+
+<style lang="scss">
+.content {
+  padding: 20rpx;
+  text-align: center;
+  margin-top: 100rpx;
+  color: #666;
+}
+</style>
