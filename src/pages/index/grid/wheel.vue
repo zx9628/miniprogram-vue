72c223d641d6991e5fcf6e91bc6453873a08e9e7
25927 <2592769572@qq.com>
2026-07-13 11:56:52 +0800

    首页跳转，及各个简陋的跳转页面

---
+++ src/pages/index/grid/wheel.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="content">
+    <text>幸运大转盘页面正在开发中...</text>
+  </view>
+</template>
+
+<script setup>
+// 抽奖逻辑后续在这里写
+</script>
+
+<style lang="scss">
+.content {
+  padding: 20rpx;
+  text-align: center;
+  margin-top: 100rpx;
+  color: #666;
+}
+</style>
