c0bffb94105cc72449a749bedde44129664af15b
25927 <2592769572@qq.com>
2026-07-14 18:03:40 +0800

    1

---
+++ src/pages/cart/cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="container">
+    <text>购物车页面开发中...</text>
+  </view>
+</template>
+
+<script setup lang="ts">
+// 这里可以留空，或者写逻辑
+</script>
+
+<style scoped>
+.container {
+  padding: 20rpx;
+  text-align: center;
+}
+</style>
